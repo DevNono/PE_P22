639640cead044bb071fe5ca5fbb4e1bc15a2ef7c
Younsss <younes.boutkrida@utt.fr>
2022-05-08 21:31:30 +0200

    1er design Modules

--- routes/modules.js
+++ routes/modules.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-	res.render('index', {title: 'Modules'});
+	res.render('modules', {title: 'Modules'});
 });
 
 module.exports = router;
