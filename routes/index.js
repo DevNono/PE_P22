b4a4da830e1d11dd11e40fb9c29dcfb7f24a78f8
Noé Landré <noe.landre@laposte.net>
2022-05-17 19:59:26 +0200

    feat: start email integration

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,28 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
+const MailazyClient = require('mailazy-node');
+const client = new MailazyClient({accessKey: '', accessSecret: ''});
+
 /* GET home page. */
 router.get('/', (req, res) => {
 	res.render('index', {title: 'Express'});
+});
+
+router.post('/contact', async (req, res) => {
+	const {name, email, message} = req.body;
+	try {
+		const resp = await client.send({
+			to: process.env.EMAIL_ADDRESS, // Required
+			from: email, // Use domain you verified, required
+			subject: '', // Required
+			text: message,
+			html: message,
+		});
+		console.log('resp: ' + resp);
+	} catch (e) {
+		console.log('errror: ' + e);
+	}
 });
 
 router.get('/health', (req, res) => {
