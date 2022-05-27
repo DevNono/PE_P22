7811dc7e7d2472c6a6113ae66ce2b6f1e201b9b8
Noé Landré <noe.landre@laposte.net>
2022-05-27 11:57:41 +0200

    fix: remove console.log

--- app/sequelize.js
+++ app/sequelize.js
@@ -1,32 +1,5 @@
 const dotenv = require('dotenv').config();
 
-console.log({
-	development: {
-		username: process.env.MYSQLUSER,
-		password: process.env.MYSQLPASSWORD,
-		database: process.env.MYSQLDATABASE,
-		host: process.env.MYSQLHOST,
-		port: process.env.MYSQLPORT,
-		dialect: 'mysql',
-	},
-	test: {
-		username: process.env.MYSQLUSER,
-		password: process.env.MYSQLPASSWORD,
-		database: process.env.MYSQLDATABASE,
-		host: process.env.MYSQLHOST,
-		port: process.env.MYSQLPORT,
-		dialect: 'mysql',
-	},
-	production: {
-		username: process.env.MYSQLUSER,
-		password: process.env.MYSQLPASSWORD,
-		database: process.env.MYSQLDATABASE,
-		host: process.env.MYSQLHOST,
-		port: process.env.MYSQLPORT,
-		dialect: 'mysql',
-	},
-});
-
 module.exports = {
 	development: {
 		username: process.env.MYSQLUSER,
